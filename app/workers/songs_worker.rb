18ad51906cf3831a2f4912b7d54656e3ae0b8900
Matt Farmer <mrfarmer777@gmail.com>
2018-07-30 22:13:58 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -3,7 +3,7 @@ class SongsWorker
   require 'csv'
 
   def perform(songs_file)
-    CSV.foreach(songs_file), headers: true do |song|
+    CSV.foreach(songs_file, headers: true) do |song|
       artist=Artist.find_or_create_by(name: song[1])
       artist.songs.build(title: song[0])
       artist.save
